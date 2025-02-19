53bafbb695aeda32e81af591d0d29c4f4569bfa3
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-02-19 16:47:04 -0600

    basic config for run the microservice

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import fastify from "fastify"
+
+const server = fastify({logger: true})
+
+server.get("/", async (request, reply) => {
+    reply.send({message: "Hello World", version: "1.0.0", author: "TRIYAN-SALAZAR", description: "API REST con Fastify"})
+})
+
+server.listen({port: 3000}, (err, address) => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    
+    console.log(`Server listening on ${address}`)
+})
+
+export default server
