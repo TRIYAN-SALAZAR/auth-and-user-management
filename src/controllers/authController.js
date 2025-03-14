7dcf354b71ce2214a01c3ad619fba0b423b61799
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-03-14 11:06:58 -0600

    Implemetation Register and Login User

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,11 +5,28 @@ import authService from '../services/authService.js';
 const { login, registerEmail } = authService;
 
 async function loginController(request, reply) {
-    reply.send({ message: "Auth login" });
+    try {
+        const { email, password } = request.body;
+        const token = await login(email, password);
+        reply.send({ message: "Auth login", token });
+    } catch (error) {
+        // reply.send({ message: error.message });
+        console.error(error);
+    }
 }
 
 async function registerController(request, reply) {
-    reply.send({ message: "Auth register" });
+    try {
+        const { email, password, firstName, lastName, age } = request.body;
+        const token = await registerEmail({ email, password, firstName, lastName, age });
+
+        if (!token) throw new Error('Error to register');
+
+        reply.send({ message: "Auth register", token });
+    } catch (error) {
+        // reply.send({ message: error.message });
+        console.error(error);
+    }
 }
 
 export default { loginController, registerController };
--- src/schemas/userShema.js
+++ src/schemas/userShema.js
@@ -5,24 +5,33 @@ import { DataTypes } from "sequelize";
 function User(sequelize) {
     const User = sequelize.define('User', {
         id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            unique: true
         },
-        name: {
-        type: DataTypes.STRING,
-        allowNull: false
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         },
         email: {
-        type: DataTypes.STRING,
-        allowNull: false
+            type: DataTypes.HSTORE,
+            allowNull: false,
+            unique: true
         },
         password: {
-        type: DataTypes.STRING,
-        allowNull: false
+            type: DataTypes.STRING,
+            allowNull: false
         }
     });
-    
+
     return User;
 }
 
