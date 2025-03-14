bb2d9f9eeb90ec7e7bb71b73c5cf6eae5f605801
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-03-14 11:21:59 -0600

    Changed the name crypt by Hash

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,21 +3,21 @@
 import User from '../schemas/userShema.js';
 import jwt from '../utils/jwt.js';
 import IDs from '../utils/idGenerate.js';
-import crypt from '../utils/hash.js';
+import Hash from '../utils/hash.js';
 
 
 async function login(email, password) {
     const user = await User.findOne({ where: { email } });
     const hash = user.password;
 
-    if(!user || !crypt.checkHashToPassword(password, hash)) throw new Error('Invalid email or password');
+    if(!user || !Hash.checkHashToPassword(password, hash)) throw new Error('Invalid email or password');
 
     return jwt.sign({ id: user.id });
 }
 
 async function registerEmail(dataUser) {
     const { email, password, firstName, lastName, age } = dataUser;
-    const hash =  crypt.hashPassword(password);
+    const hash =  Hash.hashPassword(password);
     const id =  IDs.generate();
     const user = await User.create({ id, email, password: hash, first_name: firstName, last_name: lastName, age });
 
