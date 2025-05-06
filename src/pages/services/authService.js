03453bc451f7b25c575bab1c735680b82bc04ed5
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-05-06 10:43:45 -0600

    Fix: Error when trying to add a new User.

--- src/pages/services/authService.js
+++ src/pages/services/authService.js
@@ -1,13 +1,13 @@
 'use strict';
 
-import User from '../../schemas/userShema.js';
+import UserSchema from '../../server.js';
 import jwt from '../../utils/jwt.js';
 import IDs from '../../utils/idGenerate.js';
 import Hash from '../../utils/hash.js';
 
 async function login(email, password) {
     try {
-        const user = await User.findOne({ where: { email } });
+        const user = await UserSchema.findOne({ where: { email } });
         const hash = user.password;
 
         if (!user || !Hash.checkHashToPassword(password, hash)) throw new Error('Invalid email or password');
@@ -28,7 +28,7 @@ async function registerEmail(dataUser) {
         const { email, password, firstName, lastName, age } = dataUser;
         const hash = Hash.hashPassword(password);
         const id = IDs.generate();
-        const user = await User.create({ id, email, password: hash, first_name: firstName, last_name: lastName, age });
+        const user = await UserSchema.create({ id, email, password: hash, first_name: firstName, last_name: lastName, age });
 
         return jwt.sign({ id: user.id });
 
