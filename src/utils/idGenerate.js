f1f37b06b6914afd24608299a8288ce348e881b1
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-05-06 12:05:18 -0600

    Update: change name function

--- src/utils/idGenerate.js
+++ src/utils/idGenerate.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import {v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
-function generate() {
+function generateID() {
     return uuidv4()
 }
 
-export default {generate};
+export { generateID };
