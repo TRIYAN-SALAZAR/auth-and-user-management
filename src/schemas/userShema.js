4517c57da4574b7481a0db535bab9108784cd017
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-05-23 20:59:55 -0600

    fix: ensure email field is unique in User schema definition

--- src/schemas/userShema.js
+++ src/schemas/userShema.js
@@ -26,6 +26,7 @@ function UserSchema(sequelize) {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true
         },
         password: {
             type: DataTypes.STRING,
