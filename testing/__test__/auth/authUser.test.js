5cd307fcc2befdfaebeb86c1d6f4c63590af84e4
Jorge Ozuna <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2025-07-10 23:37:22 -0600

    test: refactor authUser tests to use async/await and improve user creation

--- testing/__test__/auth/authUser.test.js
+++ testing/__test__/auth/authUser.test.js
@@ -1,21 +1,45 @@
-import server from '../../../src/app.js';
+import fastify from 'fastify';
+import app from '../../../src/app.js';
 import request from 'supertest';
 import User from '../../dbConnector.js';
+import hash from '../../hash.js';
+import { where } from 'sequelize';
 
-beforeAll(() => {
-    const user = User.build({
+const email = 'test@mail.com';
+const password = 'UnPato32LLamado_Juan';
+let server, userId;
+
+beforeAll(async () => {
+    server = app
+    await server.ready()
+
+    await User.create({
         first_name: 'Jorge',
         last_name: 'Ozuna',
         age: 20,
-        email: 'test@mail.com',
-        password: 'aaaaaa'
+        email,
+        password: hash.hashPassword(password)
     });
 
-    return user.save().then(data => data).catch(err => err);
+    // bug, resolve thus
+    userId = await User.findOne({where: {email}});
+});
+console.log(userId)
+
+afterAll(async () => {
+    await User.destroy({ where: { email } });
+    await server.close();
 });
 
-describe('POST/ auth', function() {
-    test("prueba 1", () => {
-        expect(2).toEqual(2);
+describe('POST/ auth', function () {
+    test('Login', () => {
+        const response = request(server.server)
+            .post('/login')
+            .send({ email, password, userId })
+            .set('Content-Type', 'application/json')
+            .then(data => data)
+            .catch(err => err)
+
+        expect(response.statusCode).toBe(200);
     });
 });
